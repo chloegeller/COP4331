6ef71257bcdca10b46b3aa24e9ecd1892018d00b
Jonathan Rohrback <Jon@jonathans-mbp.home>
2020-01-30 23:29:56 -0500

    Add complete landing page for App.js

--- UKnightApp/Project/App.js
+++ UKnightApp/Project/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Changed this I can test commit and pushing</Text>
+    <View style={styles.container1}>
+      <Text style={styles.heading}>UKnight</Text>
+      <View style={styles.container2}>
+        <Image style={styles.logo} source={require('./assets/knightro_logo.jpg')} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc904',
     alignItems: 'center',
+    padding: 10,
+  },
+
+  container2: {
+    backgroundColor: 'black',
+    height: 400,
+    width: 300,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  heading: {
+    fontSize: 72,
+    paddingTop: 48,
+    margin: 48,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+
+  logo: {
+    height: 300,
+    width: 250,
   },
 });
