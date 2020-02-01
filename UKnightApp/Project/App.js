8c1d4b45bc99227b019a10750606e6f19b25c288
Andrew Borg <andrewborg1126@gmail.com>
2020-02-01 16:56:16 -0500

    Removed style sheet from app.js because it is not needed there.

--- UKnightApp/Project/App.js
+++ UKnightApp/Project/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './components/HomeScreen';
@@ -19,34 +19,3 @@ export default class App extends React.Component {
     return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  container1: {
-    flex: 1,
-    backgroundColor: '#ba9b37',
-    alignItems: 'center',
-    padding: 10,
-  },
-
-  container2: {
-    backgroundColor: 'black',
-    height: 400,
-    width: 300,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  heading: {
-    fontSize: 72,
-    paddingTop: 48,
-    margin: 48,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'white',
-  },
-
-  logo: {
-    height: 300,
-    width: 250,
-  },
-});
