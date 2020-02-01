de2ac63962b8cb90f84a3130051e51caa2c4623c
Andrew Borg <andrewborg1126@gmail.com>
2020-02-01 16:50:54 -0500

    Set up a home screen that loads when the landing screen is tapped anywhere. Created buttons, without any function thus far.

--- UKnightApp/Project/App.js
+++ UKnightApp/Project/App.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './components/HomeScreen';
+import LandingScreen from './components/LandingScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Changed this I can test commit and pushing</Text>
-    </View>
-  );
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+  Land: LandingScreen,
+},{
+  initialRouteName: 'Land',
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ba9b37',
     alignItems: 'center',
+    padding: 10,
+  },
+
+  container2: {
+    backgroundColor: 'black',
+    height: 400,
+    width: 300,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  heading: {
+    fontSize: 72,
+    paddingTop: 48,
+    margin: 48,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+
+  logo: {
+    height: 300,
+    width: 250,
   },
 });
