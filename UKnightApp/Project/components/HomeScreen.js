7d2863c40258b1bfe8c44c24bf68ac0d5766b68b
h3rmi0n3 <chloegeller@knights.ucf.edu>
2020-02-20 16:21:51 -0500

    Added Notification, Settings, and Profile screens

--- UKnightApp/Project/components/HomeScreen.js
+++ UKnightApp/Project/components/HomeScreen.js
@@ -1,58 +1,94 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home',
-    headerTintColor: '#ffffff',
-    headerStyle: {
-      backgroundColor: '#000000',
-    },
-    headerRight: () => (
-      <Button
-        onPress={() => alert('This will bring you to a log in screen once we have one.')}
-        title="Log in"
-        color="#808080"
-      />
-    )
-  };
+    static navigationOptions = {
+        title: 'Home',
+        headerTintColor: '#ffffff',
+        headerStyle: {
+            backgroundColor: '#283747'
+        },
+        headerRight: () => (<Button
+            onPress={() => alert('This will bring you to a log in screen once we have one.')}
+            title="Log in"
+            color="#808080"/>)
+    };
 
-  render() {
-    return (
-      <View style = {{ flex: 1, flexDirection: 'column', backgroundColor: '#ba9b37'}}>
-        <View style = {{ flex: 1, flexDirection: 'row'}}>
-          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Home')}>
-              <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Home')}>
-              <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Home')}>
-              <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Home')}>
-              <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
-          </TouchableOpacity>
-        </View>
-        <View style = {{ flex: 6}}>
-          {/*This one stops the buttons from stretching to the bottom of the screen*/}
-        </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    leftComponent={< Icon name = "menu" onPress = {
+                    () => this
+                        .props
+                        .navigation
+                        .openDrawer()
+                } />}/>
+                <View
+                    style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <Text>Home</Text>
+                    <View
+                        style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        backgroundColor: '#ba9b37'
+                    }}>
+                        <View
+                            style={{
+                            flex: 1,
+                            flexDirection: 'row'
+                        }}>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate('Home')}>
+                                <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate('Home')}>
+                                <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate('Home')}>
+                                <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.button}
+                                onPress={() => this.props.navigation.navigate('Home')}>
+                                <Text style={styles.buttonText}>Button text{"\n\n"}Replace with image</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={{
+                            flex: 6
+                        }}>
+                            {/*This one stops the buttons from stretching to the bottom of the screen*/}
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  button: {
-    flex: 1,
-    margin: 10,
-    alignItems: 'center',
-    backgroundColor: '#505050'
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: 'white'
-  }
+    button: {
+        flex: 1,
+        margin: 10,
+        alignItems: 'center',
+        backgroundColor: '#505050'
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: 'white'
+    },
+    container: {
+        flex: 1
+    }
 });
 
 export default HomeScreen;
--- UKnightApp/Project/components/LandingScreen.js
+++ UKnightApp/Project/components/LandingScreen.js
@@ -1,56 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback } from 'react-native';
+import {StyleSheet, Text, View, Image, TouchableWithoutFeedback} from 'react-native';
 
 class LandingScreen extends React.Component {
-  static navigationOptions = {
-    title: '',
-    headerShown: false,
-  };
+    static navigationOptions = {
+        title: '',
+        headerShown: false
+    };
 
-  render() {
-    return (
-      <TouchableWithoutFeedback onPress={() => this.props.navigation.replace('Home')}>
-        <View style={styles.container1}>
-          {/* <Text style={styles.heading}>UKnight</Text> */}
-          {/* <View style={styles.container2}> */}
-            <Image style={styles.logo} source={require('../assets/UKnight_icon5.png')} />
-          </View>
-        {/* </View> */}
-      </TouchableWithoutFeedback>
-    );
-  }
+    render() {
+        return (
+            <TouchableWithoutFeedback onPress={() => this.props.navigation.replace('Home')}>
+                <View style={styles.container1}>
+                    {/* <Text style={styles.heading}>UKnight</Text> */}
+                    {/* <View style={styles.container2}> */}
+                    <Image style={styles.logo} source={require('../assets/UKnight_icon5.png')}/>
+                </View>
+                {/* </View> */}
+            </TouchableWithoutFeedback>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container1: {
-    flex: 1,
-    backgroundColor: '#283747',
-    alignItems: 'center',
-    padding: 30,
-  },
+    container1: {
+        flex: 1,
+        backgroundColor: '#283747',
+        alignItems: 'center',
+        padding: 30
+    },
 
-  // container2: {
-  //   backgroundColor: 'black',
-  //   height: 400,
-  //   width: 300,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
+    // container2: {   backgroundColor: 'black',   height: 400,   width: 300,
+    // justifyContent: 'center',   alignItems: 'center', }, heading: {   fontSize:
+    // 72,   paddingTop: 48,   margin: 48,   fontWeight: 'bold',   textAlign:
+    // 'center',   color: 'white', },
 
-  // heading: {
-  //   fontSize: 72,
-  //   paddingTop: 48,
-  //   margin: 48,
-  //   fontWeight: 'bold',
-  //   textAlign: 'center',
-  //   color: 'white',
-  // },
-
-  logo: {
-    height: 540,
-    width: 250,
-    alignItems: 'center',
-  }
+    logo: {
+        height: 540,
+        width: 250,
+        alignItems: 'center'
+    }
 });
 
 export default LandingScreen;
