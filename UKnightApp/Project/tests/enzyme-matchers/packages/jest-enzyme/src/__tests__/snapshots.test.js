22e70de221ad290d2da4866398648ece08fe8231
h3rmi0n3 <chloegeller@knights.ucf.edu>
2020-02-22 18:49:12 -0500

    deleted enzyme-matchers

--- UKnightApp/Project/tests/enzyme-matchers/packages/jest-enzyme/src/__tests__/snapshots.test.js
+++
@@ -1,9 +0,0 @@
-describe('snapshotting', () => {
-  [mount, shallow].forEach(builder => {
-    it(`serializes enzyme objects (${builder.name})`, () => {
-      const wrapper = builder(<div className="foo">Hi</div>);
-
-      expect(wrapper).toMatchSnapshot();
-    });
-  });
-});
